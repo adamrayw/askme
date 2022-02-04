7ca1d1f9596744b5558f5a7b35b97950ff6f6612
Adam Ray <raywibowo68@gmail.com>
2022-02-04 14:02:49 +0700

    Change to className

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -7,29 +7,29 @@ export default function result() {
         <div>
             <Header />
             <section
-                class="out-feature text-gray-600 bg-white rounded-lg bg-opacity-50 shadow-sm my-4 py-5 md:px-10 px-5"
+                className="out-feature text-gray-600 bg-white rounded-lg bg-opacity-50 shadow-sm my-4 py-5 md:px-10 px-5"
             >
-                <p class="mb-4 text-xl font-bold text-center">
+                <p className="mb-4 text-xl font-bold text-center">
                     Your link created successfully!
                 </p>
-                <p class="list-disc tracking-widest px-5 text-center">
+                <p className="list-disc tracking-widest px-5 text-center">
                     Now you can copy the link and paste it in your favorite social media
                     bio
                 </p>
-                <div class="my-6">
+                <div className="my-6">
                     <input
                         type="text"
                         id="name"
                         name="name"
-                        class="base-input bg-gray-300 text-center font-semibold transition-all border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full px-6 py-3 placeholder-gray-800"
+                        className="base-input bg-gray-300 text-center font-semibold transition-all border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full px-6 py-3 placeholder-gray-800"
                         placeholder="Whats your name?"
                         value="https://askme.id/awd123"
                         required
                     />
-                    <div class="flex justify-center">
+                    <div className="flex justify-center">
                         <button
                             type="submit"
-                            class="mt-8 text-white transition-all bg-gray-700 hover:bg-gray-600 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-base px-5 py-2.5 text-center"
+                            className="mt-8 text-white transition-all bg-gray-700 hover:bg-gray-600 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-base px-5 py-2.5 text-center"
                         >
                             Open in new tab
                         </button>
