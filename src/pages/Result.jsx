958a786172872e51d161d45361b14aafeb9723ec
Adam Ray <raywibowo68@gmail.com>
2022-02-05 19:30:31 +0700

    Change name of Header component

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
-import Header from '../components/header';
+import Header from '../components/Header';
 
 
 export default function Result() {
