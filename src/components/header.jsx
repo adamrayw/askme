809c8aa185f0d3f66f7179b07ed5c7ccad5129c8
Adam Ray <raywibowo68@gmail.com>
2022-02-05 19:24:40 +0700

    Add link to root route

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function header() {
     return (
         <section className="header text-center">
-            <div
+            <Link to="/"
                 className="w-full text-gray-600 p-4 bg-white bg-opacity-50 flex justify-center rounded-lg shadow-sm items-center space-x-4"
             >
                 <i className="fas fa-user-secret fa-lg"></i>
                 <h2 className="text-2xl font-bold tracking-wider">Askme</h2>
-            </div>
+            </Link>
         </section>
     )
 }
